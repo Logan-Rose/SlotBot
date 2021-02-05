d650a3979b869bb72cb39ee7d4379bf0ca3f1c55
Logan-Rose <logan.rose0329@gmail.com>
2021-02-04 23:20:38 -0500

    table now fills with user data, added User object

---
+++ public/User.js
@@ -0,0 +1,48 @@
+class User{
+    constructor(checkedIn, email, name ){
+        this.checkedIn = checkedIn;
+        this.email = email;
+        this.name = name;
+        this.preferences = [];
+        this.scratches = [];
+    }
+
+    scratch(scratchedUser){
+        this.scratches.push(scratchedUser)
+    }
+    
+    updatePreference(updatedUser){
+        for(let i =0; i < this.preferences.length; i++){
+            if(this.preferences[i].email == updatedUser.email){
+                this.preferences[i] = updatedUser
+                return;
+            }
+        }
+        this.scratches.push(updatedUser)
+    }
+    getPreferences(){
+        return this.preferences
+    }
+    getScratches(){
+        return this.scratches
+    }
+    scratched(user){
+        for(let i =0; i < this.scratches.length; i++){
+            if(this.scratches[i].email == user.email){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    isScratchedBy(user){
+        let otherScratches =user.getScratches();
+        for(let i =0; i < otherScratches.length; i++){
+            if(otherScratches[i].email == this.email){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
--- public/preferences/preferences.js
+++ public/preferences/preferences.js
@@ -1,49 +1,28 @@
+
 auth.onAuthStateChanged(function(user) {
     if (user) {
-        let users = []
+        let users = [];
         db.collection("users").get().then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 users.push(doc.data())
             });
-            console.log(users[0])
-        });
-        
-        console.log(users)
+            fillTable(users);
+        })
     }else {
       // No user is signed in.
     }
   });
-function signOut(){
-    firebase.auth().signOut().then(() => {
-        window.location = '../index.html';
-    }).catch((error) => {
-        console.log("Sign out failed");
+
+function fillTable(users){
+    console.log(users.length);
+    let table = document.getElementById('slotting');
+    users.forEach(function(user) {
+        
+        let tr = document.createElement('tr');
+        tr.innerHTML = 
+            '<td>' + user.name + '</td>' +
+            '<td>' + user.email + '</td>' +
+            '<td>' + user.checkedIn + '</td>';
+        table.appendChild(tr);
     });
 }
-
-let test = [
-    {name: "Logan", rank: 1, scratched: false},
-    {name: "Vasu", rank: 2, scratched: true},
-    {name: "James", rank: 3, scratched: true},
-]
-
-let table = document.getElementById('slotting');
-test.forEach(function(object) {
-    let tr = document.createElement('tr');
-    tr.innerHTML = 
-        '<td>' + object.name + '</td>' +
-        '<td>' + object.rank + '</td>' +
-        '<td>' + object.scratched + '</td>';
-    table.appendChild(tr);
-});
-
-
-/**
- * if admin
- * document.getElementById("myP").style.visibility = "visible";
- * else
- * document.getElementById("myP").style.visibility = "hidden";
- * 
- */
-
-
